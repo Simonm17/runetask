739f6e071d9b1f0a01265ea48fc2aec35090e628
Ryan Chung <ryanchuung@gmail.com>
2020-12-04 22:50:33 -0800

    created Task model only

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    description = models.CharField(max_length=250)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.created_by}, {self.description}'
