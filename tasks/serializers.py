e61673b754831a34fa3749289a593c634a96ffaf
Ryan Chung <ryanchuung@gmail.com>
2020-12-05 11:19:42 -0800

    created Task ModelSerializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,3 +1,11 @@
 from rest_framework import serializers
 
 from django.contrib.auth.models import User
+from .models import Task
+
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = ['description', 'created_by', 'created_date', 'updated_date']
