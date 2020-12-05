b6a04739455deb5cd239596fe9d9d56d878cb5cb
Ryan Chung <ryanchuung@gmail.com>
2020-12-05 11:38:17 -0800

    created test dir for Task app tests, fixed config url routing for tasks, and created Task model test

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('nimda/', admin.site.urls),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('tasks/', include(tasks.urls)),
+    path('tasks/', include('tasks.urls')),
 ]
