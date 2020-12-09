f32c0a091d8ce132962630e1ef91fb96cc4d9945
Ryan Chung <ryanchuung@gmail.com>
2020-12-09 10:37:56 -0800

    added comments in task/users.urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,12 @@ from django.urls import path, include
 from .api import viewsets
 
 
+# how to use when reverse():
+# e.g. reverse('tasks:detail')
+# how to use in django templates:
+# e.g. {% url 'tasks:detail' %}
+app_name = 'tasks'
+
 """ Notes:
     - Routers are used for ViewSets.
     - If using generic api views, you can use regular urlpatterns,
--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,12 @@ from django.urls import path, include
 from .api import viewsets
 
 
+# how to use when reverse():
+# e.g. reverse('users:detail')
+# how to use in django templates:
+# e.g. {% url 'users:detail' %}
+app_name = 'users'
+
 router = routers.DefaultRouter()
 router.register(r'', viewsets.UserViewSet)
 
