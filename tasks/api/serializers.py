aaee71f44c1fd8788943d1acc5591be9641156aa
Ryan Chung <ryanchuung@gmail.com>
2020-12-09 15:26:45 -0800

    added extra_kwargs to serializer Meta classes to fix error after adding namespaces

--- tasks/api/serializers.py
+++ tasks/api/serializers.py
@@ -5,7 +5,19 @@ from ..models import Task
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    # NOTE: replaced by extra_kwargs
+    # created_by = serializers.HyperlinkedIdentityField(
+    #   view_name='users:user-detail'
+    # )
 
     class Meta:
         model = Task
         fields = ['url', 'description', 'uuid', 'created_by', 'created_date', 'updated_date']
+        extra_kwargs = {
+            # need to overwrite default view_name='task-detail' because app_name
+            'url': {'view_name': 'tasks:task-detail'},
+            # need to also overwrite created_by view_name to return assigned user
+            # NOTE: if you use serializer.HyperlinkedIdentityField outside of meta,
+            #       it will assign to wrong user and instead index the user correlating to task id.
+            'created_by': {'view_name': 'users:user-detail'}
+        }
--- users/api/serializers.py
+++ users/api/serializers.py
@@ -3,13 +3,25 @@ from rest_framework import serializers
 from tasks.models import Task
 from tasks.api.serializers import TaskSerializer
 
-class UserSerializer(serializers.ModelSerializer):
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    # NOTE: lines 10-17 can be replaced with using extra_kwargs in class Meta.
     # using TaskSerializer() instead of HyperlinkedModelSerializer
     # https://stackoverflow.com/questions/20550598/django-rest-framework-could-not-resolve-url-for-hyperlinked-relationship-using
-    tasks = TaskSerializer(many=True, read_only=True)
+    # tasks = TaskSerializer(many=True, read_only=True)
+    # tasks = serializers.HyperlinkedRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     view_name='tasks:task-detail',
+    # )
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'tasks']
+        fields = ['url', 'id', 'username', 'tasks']
+        # need to overwrite default view_name='user-detail' because app_name
+        extra_kwargs = {
+            'url': {'view_name': 'users:user-detail'},
+            'tasks': {'view_name': 'tasks:task-detail'}
+        }
 
