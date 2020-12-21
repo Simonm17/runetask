e0ea1515de451d9df4187546a5672a6b5969ed73
Ryan Chung <ryanchuung@gmail.com>
2020-12-20 18:09:28 -0800

    modified useState variables to make more readable

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -4,20 +4,19 @@ import axios from 'axios';
 
 const UserPage = ({ username }) => {
 
+    const [taskUrls, setTaskUrls] = useState([]);
     const [tasks, setTasks] = useState([]);
-    const [desc, setDesc] = useState([]);
 
-    const taskDescription = desc.map( task => 
-        <li key={desc.indexOf(task)}>{task}</li> 
+    const taskDescriptions = tasks.map( task => 
+        <li key={tasks.indexOf(task)}>{task}</li> 
     );
-
 
     useEffect(() => {
         const getUserAPI = () => {
             axios.get(`http://localhost:8000/users/` + username)
             .then(res => {
                 console.log(res);
-                setTasks(res.data.tasks);
+                setTaskUrls(res.data.tasks);
                 console.log(`printing tasks: ${tasks}.`);
                 console.log(typeof tasks + '<- type');
             })
@@ -29,23 +28,21 @@ const UserPage = ({ username }) => {
     }, []);
 
     useEffect(() => {
-        console.log(`hi, tasks triggered this.`);
-        for (let i = 0; i < tasks.length; i++){
-            axios.get(tasks[i])
+        for (let i = 0; i < taskUrls.length; i++){
+            axios.get(taskUrls[i])
             .then( res => {
                 console.log(res.data.description);
                 console.log(`printed task ${i}.`);
-                setDesc(prevDesc => [...prevDesc, res.data.description]);
+                setTasks(prevTasks => [...prevTasks, res.data.description]);
             })
         }
-    }, [tasks]);
-
+    }, [taskUrls]);
 
     return (
         <div>
             <h1>{username}</h1>
-            {desc.length > 0 ?
-                taskDescription :
+            {tasks.length > 0 ?
+                taskDescriptions :
                 <p>You have no tasks! Click to add.</p>
             }
         </div>
