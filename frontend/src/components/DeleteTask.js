88656c95b283171fd0a7a576b21ff0a41defdc11
Ryan Chung <ryanchuung@gmail.com>
2020-12-23 00:45:44 -0800

    consolidated User useStates into link and task object. need to config buttons

--- frontend/src/components/DeleteTask.js
+++ frontend/src/components/DeleteTask.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import axios from 'axios';
+
 
 const DeleteTask = ({ taskUrl }) => {
     const token = localStorage.getItem('token');
 
-    
+    const handleDelete = () => {
+        if (token) {
+            // FIX AXIOS POST PARAMETERS
+            return axios.post(taskUrl, token)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+        return console.log('no token!');
+    }
+
+
+    return (
+        <button onClick={handleDelete}>
+            confirm
+        </button>
+    )
 }
 
 export default DeleteTask;
--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import DeleteTask from '../components/DeleteTask';
 
 const UserPage = ({ username }) => {
 
     const [taskUrls, setTaskUrls] = useState([]);
     const [tasks, setTasks] = useState([]);
-
-    const deletebutton = () => {
-
-    }
 
     // note: MAKE SURE USER IS AUTHENTICATED AND EQUALS OBJECT OWNER TO DISPLAY BUTTONS
     // ✓ button is a toggle; saves Task.completed field as true or false, indicated by regular or crossed-out text.
@@ -17,21 +13,21 @@ const UserPage = ({ username }) => {
     // confirm button triggers Task.delete() via post request.
     // cancel button resets buttons and returns x button.
 
-    const taskDescriptions = tasks.map( task => {
+    const getTaskInfo = tasks.map( task => {
             if (username) {
                 return <li key={tasks.indexOf(task)}>
-                    {task} 
+                    {task.description} 
+                    
                     <button>✓</button><button>&times;</button>
-                    <button>confirm</button><button>cancel</button>
+                    <DeleteTask taskUrl={taskUrls[task]} /><button>cancel</button>
                 </li> 
             }
             else {
                 return <li key={tasks.indexOf(task)}>
-                    {task}
+                    {task.description}
                 </li>
             }
         }
-
     );
 
     useEffect(() => {
@@ -51,7 +47,9 @@ const UserPage = ({ username }) => {
         for (let i = 0; i < taskUrls.length; i++){
             axios.get(taskUrls[i])
             .then( res => {
-                setTasks(prevTasks => [...prevTasks, res.data.description]);
+                console.log('PRINTING:');
+                console.log(res.data);
+                setTasks(prevTask => [...prevTask, res.data]);
             })
             .catch(err => {
                 console.log(err);
@@ -63,7 +61,7 @@ const UserPage = ({ username }) => {
         <div>
             <h1>{username}</h1>
             {tasks.length > 0 ?
-                <ul> {taskDescriptions} </ul>
+                <ul> {getTaskInfo} </ul>
                 :
                 <p>You have no tasks! Click to add.</p>
             }
