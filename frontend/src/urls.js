f3d378f8620b176c8cadd77c47db812902c7fd42
Ryan Chung <ryanchuung@gmail.com>
2021-01-16 23:15:28 -0800

    removed imports in urls.py that were auto-imported

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -1,9 +1,3 @@
-import CreateTask from "./components/CreateTask";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import TwitchLogin from "./components/TwitchLogin";
-import UserPage from "./pages/User";
-
 export const baseBackendUrl = 'http://localhost:8000'; 
 
 // Files using baseBackendUrl:
