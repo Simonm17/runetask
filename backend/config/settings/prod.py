ccc9d01ad1c90ac09a4d5bf2cbe824b39d899469
Ryan Chung <ryanchuung@gmail.com>
2021-01-18 21:36:45 -0800

    added whitenoise for deployment

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -22,4 +22,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 django_heroku.settings(locals())
