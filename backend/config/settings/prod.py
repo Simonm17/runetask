7b4e7938f4ce19c4ea5cd69d27fb80e8ca01ae14
Ryan Chung <ryanchuung@gmail.com>
2021-01-18 19:56:31 -0800

    added Procfile, static settings on prod.py, changed to prod for manage.py

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -7,7 +7,11 @@ from django.conf import settings
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    
+    'runetask-backend.herokuapp.com'
 ]
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 django_heroku.settings(locals())
