6be399e8f5ac25d754e50f5873fe4bc824ecd365
Ryan Chung <ryanchuung@gmail.com>
2021-01-19 15:42:36 -0800

    resetting static_root settings to use standard whitenoise deployment

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -16,7 +16,7 @@ CORS_ALLOWED_ORIGINS = [
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
